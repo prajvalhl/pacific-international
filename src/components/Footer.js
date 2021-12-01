e94a98c94810553f75e4770a6d0c161110e25b37
prajvalhl <prajvalhl@gmail.com>
2021-12-01 20:35:44 +0530

    refactor: added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "../styles/footer.css";
+import logo from "../img/logo.png";
 
 export function Footer() {
   return (
-    <div>
-      <h1>This is a footer</h1>
+    <div className="footer">
+      <img src={logo} alt="pacific-international-logo" className="footer-img" />
+      <p>&#169; 2021, Pacific International</p>
     </div>
   );
 }
